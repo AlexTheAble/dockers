37a546b2fe37beb9cb45c4facf4d92660f912d04
AlexTheAble <alexander.heavner@gmail.com>
2016-09-13 16:18:31 -0400

    Update start-emulator.sh

--- alexanderheavner/android/scripts/start-emulator.sh
+++ alexanderheavner/android/scripts/start-emulator.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 $EMULATOR @$AVD_NAME -no-window -noaudio &
+
+OUT=`adb shell getprop init.svc.bootanim`
+RES="stopped"
+ 
+while [[ ${OUT:0:7}  != 'stopped' ]]; do
+		OUT=`adb shell getprop init.svc.bootanim`
+		echo 'Waiting for emulator to fully boot...'
+		sleep 1
+done
+ 
+echo "Emulator booted!"
