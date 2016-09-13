4b12523cb9b5cc42a9c2fac95b5a1e6232ece516
AlexTheAble <alexander.heavner@gmail.com>
2016-09-13 17:10:44 -0400

    Update start-emulator.sh

--- alexanderheavner/android/scripts/start-emulator.sh
+++ alexanderheavner/android/scripts/start-emulator.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 $EMULATOR @$AVD_NAME -no-window -noaudio &
 
-WAIT_CMD="$ANDROID_SDK/platform-tools/adb wait-for-device shell getprop sys.boot_completed"
+adb wait-for-device
 
-until $WAIT_CMD | grep -m 1 stopped; do 
-  echo "Waiting..."
-  sleep 1
+A=$(adb shell getprop sys.boot_completed | tr -d '\r')
+
+while [ "$A" != "1" ]; do
+        sleep 2
+        A=$(adb shell getprop sys.boot_completed | tr -d '\r')
 done
